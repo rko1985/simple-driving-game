b685be8981826e9ac1164a3a987ebe8b236a5afe
Russell Oyama <russelloyama@gmail.com>
2022-06-09 11:19:12 -1000

    created main menu

--- Assets/Scripts/ScoreSystem.cs
+++ Assets/Scripts/ScoreSystem.cs
@@ -9,6 +9,8 @@ public class ScoreSystem : MonoBehaviour
     [SerializeField] private TMP_Text scoreText;
     [SerializeField] private float scoreMultiplier;
 
+    public const string HighScoreKey = "HighScore";
+
     private float score;
 
     // Update is called once per frame
@@ -17,5 +19,15 @@ public class ScoreSystem : MonoBehaviour
         score += Time.deltaTime * scoreMultiplier;
 
         scoreText.text = Mathf.FloorToInt(score).ToString();
+    }
+
+    private void OnDestroy()
+    {
+        int currentHighScore = PlayerPrefs.GetInt(HighScoreKey, 0);
+
+        if(score > currentHighScore)
+        {
+            PlayerPrefs.SetInt(HighScoreKey, Mathf.FloorToInt(score));
+        }
     }
 }
